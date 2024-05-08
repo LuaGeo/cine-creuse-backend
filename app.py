0580e63c9575b3d29fb011a7ee19d9b8e6809ae2
luageo <ldeoliveiratech@gmail.com>
2024-05-08 13:52:14 +0200

    Favorites routes created

--- app.py
+++ app.py
@@ -11,13 +11,17 @@ load_dotenv()
 app = Flask(__name__)
 app.config["MONGO_URI"] = os.getenv("MONGO_URI")
 
-CORS(app)
+# CORS(app)
 CORS(app, resources={r"/api/*": {"origins": "*"}}, supports_credentials=True)
 # Initialize database
 get_db(app)
 
 # Import routes
-from routes import *
+from routes.user import setup_user_routes
+from routes.favorite_movies import setup_favorite_movies_routes
+
+setup_user_routes(app)
+setup_favorite_movies_routes(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
